d8bbbea212b8fa490451dcb3a65a3cfb3016be7f
Momavi <68790545+Momavi@users.noreply.github.com>
2023-03-22 16:28:52 +0300

    config change

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       screens: {
         oldmb: "320px",
-        preoldmb: "420px"
+        preoldmb: "420px",
       },
     },
   },
