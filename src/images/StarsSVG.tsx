0a3ea6f94b3c6681b5b0825f475d80fd6d954c88
Momavi <68790545+Momavi@users.noreply.github.com>
2023-03-22 16:28:32 +0300

    IMG

---
+++ src/images/StarsSVG.tsx
@@ -0,0 +1,14 @@
+export default function ArrowDownLeftSVG() {
+  return (
+    <svg width="26" height="24" xmlns="http://www.w3.org/2000/svg">
+      <g clip-path="url(#a)">
+        <path d="m4.817 23.926 2.046-8.846L0 9.13l9.067-.787L12.593 0l3.526 8.343 9.066.787-6.863 5.95 2.047 8.846-7.776-4.69-7.776 4.69z" />
+      </g>
+      <defs>
+        <clipPath id="a">
+          <path d="M0 0h25.778v24H0z" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
--- src/images/index.tsx
+++ src/images/index.tsx
@@ -1,32 +1,34 @@
-//SVG
-import Cloack from "./cloack.svg";
-import Email from "./email.svg";
-import Phone from "./phone.svg";
-import Place from "./place.svg";
-import Stars from "./stars.svg";
-
 //PNG
 import Bg from "./Bg.png";
 import Cat from "./Cat.png";
 
+//SVG
+import CloackSVG from "./CloackSVG";
+import EmailSVG from "./EmailSVG";
+import PhoneSVG from "./PhoneSVG";
+import PlaceSVG from "./PlaceSVG";
+import StarsSVG from "./StarsSVG";
+
 // PRICE
-import ArrowDownBold from "./price/arrow-down-left-bold.svg";
-import ArrowDown from "./price/arrow-down-left.svg";
-import Bottle from "./price/prescription-bottle.svg";
-import Scissors from "./price/scissors-fill.svg";
-import Syringe from "./price/syringe.svg";
+import ArrowDownLeftSVG from "./price/ArrowDownLeftSVG";
+import BottleSVG from "./price/BottleSVG";
+import ScissorsSVG from "./price/ScissorsSVG";
+import SyringeSVG from "./price/SyringeSVG";
+
+// NAV
+import SunSVG from "./nav/SunSVG";
 
 export {
-  Cloack,
-  Email,
-  Phone,
-  Stars,
-  Place,
   Cat,
   Bg,
-  ArrowDownBold,
-  ArrowDown,
-  Bottle,
-  Scissors,
-  Syringe,
+  CloackSVG,
+  EmailSVG,
+  PhoneSVG,
+  PlaceSVG,
+  StarsSVG,
+  ArrowDownLeftSVG,
+  BottleSVG,
+  ScissorsSVG,
+  SyringeSVG,
+  SunSVG,
 };
