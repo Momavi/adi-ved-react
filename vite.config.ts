65974dd4ef051dce6db1dced61ac1c7ad8ecd3ba
Erbis Vladislav <cool.ru1960@gmail.com>
2023-04-14 23:28:07 +0300

    изм конфиг

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/Momavi/adi-ved-react-",
-  build: {
-    outDir: "build",
-  },
+  base: "/adi-ved-react-/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
