7a4ff0e18145e9ce4008ad44a6f9cfa52a38c345
Erbis Vladislav <cool.ru1960@gmail.com>
2023-04-15 08:14:29 +0300

    Изменил цвет

--- src/pages/MedCard/MedCardPage.tsx
+++ src/pages/MedCard/MedCardPage.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function MedCardPage() {
   return (
     <div className="mt-64 flex h-full w-full items-center justify-center">
-      <div className="stats bg-primary text-primary-content">
+      <div className="stats bg-[#37daffae] text-primary-content">
         <div className="stat">
           <div className="stat-value">Скоро будет</div>
         </div>
